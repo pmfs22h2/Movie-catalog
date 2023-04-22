b51597576fa1652dbde27ef44bc60e49e0b43137
Norbert Augusztin <anorbee95@gmail.com>
2023-04-22 17:27:41 +0200

    search-bar

--- movie-catalog/src/Pages/Home/Home.jsx
+++ movie-catalog/src/Pages/Home/Home.jsx
@@ -1,30 +1,52 @@
 import { useEffect, useState } from "react";
-import { TRENDING_URL } from "../../constants/constants"
+import { useSearchParams } from "react-router-dom";
+import { TRENDING_URL, SEARCH_URL } from "../../constants/constants"
 import MovieThumbnail from "../../components/MovieThumbnail/MovieThumbnail";
 import './home.css'
 export default function Home() {
 
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchValue, setSearchValue] = useState("");
     const [movieList, setMovieList] = useState({
         results: []
     })
+
+
+    const searchMovie = async () => {
+        const searched = searchParams.get("search");
+        if(!searched) return;
+        const res = await fetch(`${SEARCH_URL}${searched}`);
+        const data = await res.json()
+        setMovieList(data);
+    }
+
+
 
     const getData = async () => {
 
         const res = await fetch(`${TRENDING_URL}`);
         const data = await res.json()
-        console.log(data)
 
         setMovieList(data)
 
     }
-    console.log(TRENDING_URL)
     useEffect(() => {
         getData();
     }, [])
+    useEffect(() => {
+        searchMovie();
+    }, [searchParams])
+
+    const searchHandler = (event) => {
+        setSearchValue(event.target.value);
+        setSearchParams({...searchParams, search: event.target.value});
+        if(!event.target.value) getData();
+    }
 
     return (
         <div>
             <h1>Movies</h1>
+            <input type="text" value={searchValue} onChange={searchHandler} />
             <div className="movies">
                 {movieList.results.map(movie => <MovieThumbnail key={movie.id} movieData={movie} />)}
             </div>
--- movie-catalog/src/constants/constants.js
+++ movie-catalog/src/constants/constants.js
@@ -3,6 +3,7 @@ export const API_KEY = "bc51ee0e27700795691afda7a2cfa061"
 export const BASE_URL = "https://api.themoviedb.org/3"
 export const TRENDING_URL = "https://api.themoviedb.org/3/trending/all/day?api_key=bc51ee0e27700795691afda7a2cfa061"
 export const MOVIES_URL = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=bc51ee0e27700795691afda7a2cfa061&language=en-US"
+export const SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=bc51ee0e27700795691afda7a2cfa061&query="
 
 /*
 
